6ef77023c333141c7cf996d6fc944d66e1a697b8
Jitesh Kamble <io@jite.sh>
2015-12-01 15:08:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Pip
+===
+
+Provides autocompletion support for [Pip][1].
+Based on [oh-my-zsh][2] pip autocompletion [plugin][2].
+
+Functions
+---------
+
+  - `_pip` supports autocompletion for pip
+  - `pip-clear-cache` clears cached list of pip packages
+
+Authors
+-------
+
+  - [Jitesh Kamble](https://github.com/hashknot)
+
+[1]: https://pip.readthedocs.org/
+[2]: http://ohmyz.sh
+[3]: https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/pip
---
+++ init.zsh
@@ -0,0 +1,17 @@
+# If you would like to clear your cache, go ahead and do a
+# "pip-clear-cache".
+
+if (( ! ( $+commands[pip]     || \
+          $+commands[pip2]    || \
+          $+commands[pip-2.7] || \
+          $+commands[pip3]    || \
+          $+commands[pip-3.2] || \
+          $+commands[pip-3.3] || \
+          $+commands[pip-3.4] || \
+       )
+    )); then
+    return 1
+fi
+
+autoload -Uz _pip
+compdef _pip pip pip2 pip-2.7 pip3 pip-3.2 pip-3.3 pip-3.4
